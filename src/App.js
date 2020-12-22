9e08a451955e7f29d7edd006ef80821357efb5e0
Chan Li Liao <cliao119@gmail.com>
2020-12-21 23:03:56 -0500

    finish add modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, Fragment } from 'react';
 import './App.css';
 import SearchBar from './component/layout/SearchBar';
 import Logs from './component/logs/Logs';
+import AddBtn from './component/layout/AddBtn';
+import AddLogModal from './component/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css';
 
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
